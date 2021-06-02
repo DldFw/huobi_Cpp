6f3a1b68b08ab7aa5b8f955817b231cd9e7bdbff
ubuntu <ubuntu@localhost.localdomain>
2021-06-02 21:29:04 +0800

    fix bugs

--- demo/account/accountdemo.cpp
+++ demo/account/accountdemo.cpp
@@ -7,25 +7,26 @@ int main() {
     vector<Account> accounts = accountClient.getAccounts();
     cout << accounts[0].type << endl;
 
-    vector<Balance> balanceVec = accountClient.getBalance(12345);
+    vector<Balance> balanceVec = accountClient.getBalance(3504844);
     cout << balanceVec[0].type << endl;
 
-    AccountHistoryRequest accountHistoryRequest{12345, "usdt"};
+   /* AccountHistoryRequest accountHistoryRequest{3504844, "usdt"};
     accountHistoryRequest.transactTypes = "trade";
     accountHistoryRequest.size = 10;
     vector<AccountHistory> accountHistory = accountClient.getHistory(accountHistoryRequest);
-    cout << accountHistory[0].currency << endl;
+    cout << accountHistory[0].currency << endl;*/
 
-    AccountLedgerRequest accountLedgerRequest{12345, "usdt"};
+    /*AccountLedgerRequest accountLedgerRequest{3504844, "usdt"};
     accountClient.getLedger(accountLedgerRequest);
 
     FuturesTransferRequest futuresTransferRequest{"usdt", "0.2", "futures-to-pro"};
-    cout << accountClient.futuresTransfer(futuresTransferRequest) << endl;
+    cout << accountClient.futuresTransfer(futuresTransferRequest) << endl;*/
 
-    vector<AccountAndBalance> accountAndBalanceVec = accountClient.getSubuidAccount(12345);
-    cout << accountAndBalanceVec[0].symbol << endl;
+   /* vector<AccountAndBalance> accountAndBalanceVec = accountClient.getSubuidAccount(3504844);
+    cout << accountAndBalanceVec[0].symbol << endl;*/
     AssetValuationRequest request;
     request.accountType = "spot";
+    request.valuationCurrency ="cny";
     AssetValuation assetValuation = accountClient.getAssetValuation(request);
     cout << "i have " << assetValuation.balance << " in " << assetValuation.timestamp << endl;
     return 0;
--- include/define.h
+++ include/define.h
@@ -13,8 +13,8 @@
 #define BUFF 40960+1024
 #define TIME_OUT 5000
 
-#define APIKEY  "xxxx"
-#define SECRETKEY "xxxx"
+#define APIKEY  "f7db8c70-gr4edfki8l-59fab8fa-d3b0f"
+#define SECRETKEY "326a0b1f-a03810f8-c6ebeefa-03287"
 
 
 #endif //HUOBI_DEFINE_H
